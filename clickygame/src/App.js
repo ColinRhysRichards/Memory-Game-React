4c22313b35b0fef3508db17cbc424fef9b074fc0
colin <colin.rhys.richards@gmail.com>
2018-09-08 15:10:02 -0600

    component files

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,21 +1,104 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Nav from './components/Nav/Nav';
+import Image from './components/Image/Image';
+import Grid from './components/Grid/Grid';
+import Jumbotron from './components/Jumbotron/Jumbotron';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+ state = {
+   grid: [
+     {
+       name: "Image 1",
+       url: "https://www.yo.com/photos/eroiwjet",
+       clicked: false
+     },
+     {
+       name: "Image 1",
+       url: "https://www.yo.com/photos/eroiwjet",
+       clicked: false
+     },
+     {
+       name: "Image 1",
+       url: "https://www.yo.com/photos/eroiwjet",
+       clicked: false
+     },
+     {
+      name: "Image 1",
+      url: "https://www.yo.com/photos/eroiwjet",
+      clicked: false
+    },
+    {
+      name: "Image 1",
+      url: "https://www.yo.com/photos/eroiwjet",
+      clicked: false
+    },
+    {
+      name: "Image 1",
+      url: "https://www.yo.com/photos/eroiwjet",
+      clicked: false
+    },
+    {
+      name: "Image 1",
+      url: "https://www.yo.com/photos/eroiwjet",
+      clicked: false
+    },
+    {
+      name: "Image 1",
+      url: "https://www.yo.com/photos/eroiwjet",
+      clicked: false
+    },
+    {
+      name: "Image 1",
+      url: "https://www.yo.com/photos/eroiwjet",
+      clicked: false
+    },
+    {
+      name: "Image 1",
+      url: "https://www.yo.com/photos/eroiwjet",
+      clicked: false
+    },
+    {
+      name: "Image 1",
+      url: "https://www.yo.com/photos/eroiwjet",
+      clicked: false
+    },
+    {
+      name: "Image 1",
+      url: "https://www.yo.com/photos/eroiwjet",
+      clicked: false
+    }
+   ],
+   usersScore: 0,
+   name: "devyn"
+ }
+
+ clickImage = (imageObj, index) => {
+   if (!imageObj.clicked) {
+     let newGrid = this.state.grid;
+     newGrid[index].clicked = true;
+     this.setState({
+       grid: newGrid,
+       ...this.state
+     })
+   }
+ }
+
+ render() {
+   return (
+     <div className="App">
+       <Nav />
+       <div>Jumbotron</div>
+       <div>
+         {this.state.grid.map((imageObj, index) => (
+           <Image key={index} image={imageObj} onClick={() => this.clickImage(imageObj, index)} />
+         ))}
+       </div>
+     </div>
+   );
+ }
 }
 
 export default App;
